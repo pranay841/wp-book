c66ba630ccfde140352affd67bb3e508aa942c3a
Pranay Chahare <pranaychahare84@gmail.com>
2020-12-23 14:23:40 +0530

    Created a function to add custom post type book and made corrections as per wordpresss standards.

--- wp-book.php
+++ wp-book.php
@@ -1,7 +1,13 @@
 <?php
 /**
-*Plugin Name: Wp Book
-*Description: This plugin is used to store the information about books.
-*Author:      Pranay Chahare
-*Text-domain: wp-book
-**/
+ * Main file for the plugin.
+ *
+ * @package WordPress
+ * Plugin Name: Wp Book
+ * Description: This plugin is used to store the information about books.
+ * Author:      Pranay Chahare
+ * Text-domain: wp-book
+ */
+
+// loads wp-add-book-type file from includes folder.
+require dirname( __FILE__ ) . '/includes/wb-add-book-type.php';
